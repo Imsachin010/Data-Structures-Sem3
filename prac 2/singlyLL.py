d5edf627fe8777c7897122a09fdae5f7a4ac3fbf
sachin <007mishrasachinmishra@gmail.com>
2023-01-01 22:27:57 +0530

    Created a singly Link list

---
+++ prac 2/singlyLL.py
@@ -0,0 +1,10 @@
+#PERFORM CREATION,INSERTION & DELETION OPERATIONS IN SINGLY LINK LIST 
+#creating a singly link list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Ll:
+    def __init__(self):
+        self.haed= None
+
