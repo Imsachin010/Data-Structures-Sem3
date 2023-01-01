4378d0f18115cfba61ebaeb20a3e8572935a7f9a
sachin <007mishrasachinmishra@gmail.com>
2023-01-01 22:49:09 +0530

    ways of Inserting a node

--- prac 2/singlyLL.py
+++ prac 2/singlyLL.py
@@ -4,7 +4,23 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-class Ll:
+class LL:
     def __init__(self):
         self.haed= None
+
+#Insertion at front 
+def push(self, new_data):
+    new_node= Node(new_data)
+    new_node.next=self.head
+    self.head =new_node
+
+#Insertion after a given node
+
+def insert(self, prev_node, new_data):
+    if prev_node is None:
+        print("The given node must in LL ")
+        return
+    new_node = Node(new_data)
+    new_node.next = prev_node.next
+    prev_node.next = new_node
 
